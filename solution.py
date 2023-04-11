1b876914201ff20b470b21bfcf7938500a09a9f3
IrinaShel <129631109+IrinaShel@users.noreply.github.com>
2023-04-12 01:57:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import permutation_test
 
 chat_id = 5351182285 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    res = permutation_test((x, y), statistic, vectorized=True,n_resamples=np.inf, alternative='greater')
+    Result = bool (res.pvalue<=0.09)
+    return Result # Ваш ответ, True или False
